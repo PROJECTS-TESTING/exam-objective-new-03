25a04bce6f604335d8609bfec23869e63ca13cbf
PROJECTS-TESTING <33412315+PROJECTS-TESTING@users.noreply.github.com>
2018-01-16 22:36:27 +0700

    TEE04

--- Exam_Objective/Controllers/ExamBankController.cs
+++ Exam_Objective/Controllers/ExamBankController.cs
@@ -17,6 +17,7 @@ using Microsoft.Ajax.Utilities;
 using WebGrease.Css.Extensions;
 using ConsoleAppLog;
 using Networks;
+using System.Xml.Linq;
 
 namespace Exam_Objective.Controllers
 {
@@ -600,5 +601,301 @@ namespace Exam_Objective.Controllers
             }
             return Json(jsonreturn);
         }
+        // GEGIN Import
+        // Index of Import
+        public ActionResult Import()
+        {
+            var user = Session["User"] as UserSystemModel;
+            if (user == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            else if (user.Status == "student")
+            {
+                return RedirectToAction("Index", "Student");
+            }
+            using (var DB = new dbEntities())
+            {
+                var SubjectData = (from s in DB.Subjects
+                                   join u in DB.UserSystem on s.UserID equals u.UserID
+                                   where u.UserID == user.UserID
+                                   orderby s.SubjectID
+                                   select new SubjectsModel
+                                   {
+                                       SubjectID = s.SubjectID,
+                                       SubjectName = s.SubjectName,
+                                       UserID = s.UserID
+                                   }).ToList();
+                ViewBag.Subject = SubjectData;
+            }
+            using (var DB = new dbEntities())
+            {
+                var SubjectShare = (from s in DB.Subjects
+                                    join p in DB.Participant on s.SubjectID equals p.SubjectID
+                                    where p.ParticipantID == user.UserID
+                                    orderby s.SubjectID
+                                    select new SubjectsModel
+                                    {
+                                        SubjectID = s.SubjectID,
+                                        SubjectName = s.SubjectName,
+                                        UserID = p.UserID
+                                    }).ToList();
+                ViewBag.SubjectShare = SubjectShare;
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ImportMain(string SubjecID)
+        {
+            var user = Session["User"] as UserSystemModel;
+            if (user == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            else if (user.Status == "student")
+            {
+                return RedirectToAction("Index", "Student");
+            }
+            using (var DB = new dbEntities())
+            {
+                var SubjectData = (from s in DB.Subjects
+                                   join u in DB.UserSystem on s.UserID equals u.UserID
+                                   where u.UserID == user.UserID && s.SubjectID == SubjecID
+                                   orderby s.SubjectID
+                                   select new SubjectsModel
+                                   {
+                                       SubjectID = s.SubjectID,
+                                       SubjectName = s.SubjectName,
+                                       UserID = s.UserID
+                                   }).ToList();
+                ViewBag.Subject = SubjectData;
+                var SubjectShare = (from s in DB.Subjects
+                                    join p in DB.Participant on s.SubjectID equals p.SubjectID
+                                    where p.ParticipantID == user.UserID && p.SubjectID == SubjecID
+                                    orderby p.SubjectID
+                                    select new SubjectsModel
+                                    {
+                                        SubjectID = s.SubjectID,
+                                        SubjectName = s.SubjectName,
+                                        UserID = p.UserID
+                                    }).ToList();
+                ViewBag.SubjectShare = SubjectShare;
+                var UserIDData = (from u in DB.UserSystem
+                                  where u.UserID == user.UserID
+                                  orderby u.UserID
+                                  select new UserSystemModel
+                                  {
+                                      UserID = u.UserID,
+                                      Fname = u.Fname,
+                                      Lname = u.Lname,
+                                      Status = u.Status
+                                  }).ToList();
+                ViewBag.UserIDdata = UserIDData;
+                var ObjectiveDatanotDis = (from o in DB.Objective
+
+                                           let oPropos = (           //join และนับค่าที่ต้องการ
+                                           from p in DB.Proposition
+                                           where p.ObjID == o.ObjID
+                                           select o).Count()
+
+                                           select new ObjectiveModel
+                                           {
+                                               ObjID = o.ObjID,
+                                               ObjName = o.ObjName,
+                                               TextObj = o.TextObj,
+                                               PLessonID = o.LessonID,
+                                               CountProposID = oPropos
+                                           }).ToList();
+                ViewBag.ObjectiveNotDis = ObjectiveDatanotDis;
+                var ObjectiveData = (from o in DB.Objective
+                                     join l in DB.Lesson on o.LessonID equals l.LessonID
+                                     where l.SubjectID == SubjecID
+                                     orderby o.ObjID
+                                     select new ObjectiveModel
+                                     {
+                                         ObjID = o.ObjID,
+                                         ObjName = o.ObjName
+                                     }).ToList();
+                ViewBag.PObjective = ObjectiveData;
+                var ProposData = (from p in DB.Proposition
+                                  join o in DB.Objective on p.ObjID equals o.ObjID
+                                  join l in DB.Lesson on o.LessonID equals l.LessonID
+                                  // join ps in DB.PropostitionSub on p.ProposID equals ps.ProposID  where p.ProposID == ps.ProposID
+                                  where l.SubjectID == SubjecID
+                                  orderby p.ProposID
+                                  select new PropositionModel
+                                  {
+                                      ObjID = p.ObjID,
+                                      ProposID = p.ProposID,
+                                      ProposName = p.ProposName,
+                                      TextPropos = p.TextPropos,
+                                      ScoreMain = p.ScoreMain,
+                                      ObjName = o.ObjName,
+                                      Continuity = p.Continuity
+
+                                  }).ToList();
+                ViewBag.ProposMain = ProposData;
+
+                var ChoiceData = (from c in DB.Choice
+                                  join p in DB.Proposition on c.ProposID equals p.ProposID
+                                  where c.ProposID == p.ProposID
+                                  orderby c.ProposID
+                                  select new ChoiceModel
+                                  {
+                                      ChoiceID = c.ChoiceID,
+                                      ProposID = p.ProposID,
+                                      TextChoice = c.TextChoice,
+                                      Answer = c.Answer
+                                  }).ToList();
+                ViewBag.ChoiceShow = ChoiceData;
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ImportMain(HttpPostedFileBase xmlFile)
+        {
+            if (xmlFile.ContentType.Equals("application/xml") || xmlFile.ContentType.Equals("text/xml"))
+            {
+                try
+                {
+                    var xmlPath = Server.MapPath("~/Content/" + xmlFile.FileName);
+                    xmlFile.SaveAs(xmlPath);
+                    XDocument xDoc = XDocument.Load(xmlPath);
+                    List<Proposition> productList = xDoc.Descendants("PropositionModel").Select(ProductModel => new Proposition
+                    {
+                        ProposName = ProductModel.Element("ProposName").Value,
+                        TextPropos = ProductModel.Element("TextPropos").Value,
+                        ScoreMain = Convert.ToDouble(ProductModel.Element("ScoreMain").Value),
+                        CheckChoice = Convert.ToInt32(ProductModel.Element("CheckChoice").Value)
+                    }).ToList();
+                    using (dbEntities de = new dbEntities())
+                    {
+                        foreach (var i in productList)
+                        {
+                            var v = de.Proposition.Where(a => a.ProposID.Equals(i.ProposID)).FirstOrDefault();
+                            if (v != null)
+                            {
+                                v.ProposID = i.ProposID;
+                                v.ProposName = i.ProposName;
+                                v.TextPropos = i.TextPropos;
+                                v.ScoreMain = i.ScoreMain;
+                                v.CheckChoice = i.CheckChoice;
+                            }
+                            else
+                            {
+                                de.Proposition.Add(i);
+                            }
+                        }
+                        de.SaveChanges();
+                        ViewBag.Result = de.Proposition.ToList();
+                    }
+                    return View("ImportMain");
+                }
+                catch
+                {
+                    ViewBag.Error = "Can't import xml file";
+                    return View("ImportMain");
+                }
+            }
+            else
+            {
+                ViewBag.Error = "Can't import xml file";
+                return View("ImportMain");
+            }
+        }
+        [HttpGet]
+        public ActionResult Importin(int ObjID)
+        {
+            using (var DB = new dbEntities())
+            {
+                var ObjectiveData = (from o in DB.Objective
+                                     where o.ObjID == ObjID
+                                     orderby o.ObjID
+                                     select new ObjectiveModel
+                                     {
+                                         ObjID = o.ObjID,
+                                         ObjName = o.ObjName
+                                     }).ToList();
+                ViewBag.Objective = ObjectiveData;
+                return View();
+            }
+        }
+        /*[HttpPost]
+        public ActionResult Importin(HttpPostedFileBase xmlFile)
+        {
+            if (xmlFile.ContentType.Equals("application/xml") || xmlFile.ContentType.Equals("text/xml"))
+            {
+                try
+                {
+                    var xmlPath = Server.MapPath("~/Content/" + xmlFile.FileName);
+                    xmlFile.SaveAs(xmlPath);
+                    XDocument xDoc = XDocument.Load(xmlPath);
+                    List<Proposition> proposList = xDoc.Descendants("PropositionModel").Select(PropositionModel => new Proposition
+                    {
+                        ObjID = Convert.ToInt32(PropositionModel.Element("ObjID").Value),
+                        Name = ProductModel.Element("Name").Value,
+                        Price = Convert.ToDecimal(ProductModel.Element("Price").Value),
+                        Quantity = Convert.ToInt32(ProductModel.Element("Quantity").Value)
+                    }).ToList();
+                    using (DemoEntities de = new DemoEntities())
+                    {
+                        foreach (var i in productList)
+                        {
+                            var v = de.ProdectImport.Where(a => a.id.Equals(i.id)).FirstOrDefault();
+                            if (v != null)
+                            {
+                                v.id = i.id;
+                                v.Name = i.Name;
+                                v.Price = i.Price;
+                                v.Quantity = i.Quantity;
+                            }
+                            else
+                            {
+                                de.ProdectImport.Add(i);
+                            }
+                        }
+                        de.SaveChanges();
+                        ViewBag.Result = de.ProdectImport.ToList();
+                    }
+                    List<Customer> customerList = xDoc.Descendants("CustomerModel").Select(CustomerModel => new Customer
+                    {
+                        CusFname = CustomerModel.Element("CusFname").Value,
+                        CusLname = CustomerModel.Element("CusLname").Value
+                    }).ToList();
+                    using (DemoEntities de = new DemoEntities())
+                    {
+                        foreach (var i in customerList)
+                        {
+                            var v = de.Customer.Where(a => a.Id.Equals(i.Id)).FirstOrDefault();
+                            if (v != null)
+                            {
+                                v.Id = i.Id;
+                                v.CusFname = i.CusFname;
+                                v.CusLname = i.CusLname;
+
+                            }
+                            else
+                            {
+                                de.Customer.Add(i);
+                            }
+                        }
+                        de.SaveChanges();
+                        ViewBag.ResultCustomer = de.Customer.ToList();
+                    }
+                    return View("Success");
+                }
+                catch
+                {
+                    ViewBag.Error = "Can't import xml file";
+                    return View("Importin");
+                }
+            }
+            else
+            {
+                ViewBag.Error = "Can't import xml file";
+                return View("Importin");
+            }
+        }*/
     }
 }
--- Exam_Objective/Models/PropositionModel.cs
+++ Exam_Objective/Models/PropositionModel.cs
@@ -4,22 +4,32 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
+using System.Xml.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace Exam_Objective.Models
 {
+    [Serializable]
+    [XmlRoot("Propossition")]
     public class PropositionModel
     {
+        [XmlElement("proposid")]
         public int ProposID { get; set; }
+        [XmlElement("objname")]
         public int ObjID { get; set; }
+        [XmlElement("proposname")]
         public string ProposName { get; set; }
+        [XmlElement("textpropos")]
         [AllowHtml]
         [UIHint("tinymce_full_compressed")]
         public string TextPropos { get; set; }
+        [XmlElement("scoremain")]
         public double ScoreMain { get; set; }
         public Nullable<int> Continuity { get; set; }
         public Nullable<double> Difficulty { get; set; }
         public Nullable<double> Discimination { get; set; }
         public Nullable<double> IOC { get; set; }
+        [XmlElement("checkchoice")]
         public Nullable<int> CheckChoice { get; set; }
         //for join
         public string ObjName { get; set; }
@@ -47,5 +57,10 @@ namespace Exam_Objective.Models
         [UIHint("tinymce_full_compressed")]
         public string Choice5 { get; set; }
         public double Answer5 { get; set; }
+    }
+    [MetadataType(typeof(PropositionModel))]
+    public partial class Proposition
+    {
+
     }
 }
