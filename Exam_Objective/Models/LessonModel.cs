6b40d93e1b41a11ed0657868cce2ced309225d2f
PROJECTS-TESTING <33412315+PROJECTS-TESTING@users.noreply.github.com>
2018-04-11 21:21:58 +0700

    NOY40

--- Exam_Objective/Models/LessonModel.cs
+++ Exam_Objective/Models/LessonModel.cs
@@ -18,12 +18,10 @@ namespace Exam_Objective.Models
         public string UserID { get; set; }
         public string SubjectName { get; set; }  //สร้างไว้สำหรับ join
         public string Fname { get; set; }  //สร้างไว้สำหรับ join
-<<<<<<< HEAD
-=======
         public int CountObjective { get; set; }
         [AllowHtml]
         [UIHint("tinymce_full_compressed")]
         public string Contents { get; set; }
->>>>>>> 0f567a1f496edc3c2714b8e2ec92ee16e35552a2
+
     }
 }
