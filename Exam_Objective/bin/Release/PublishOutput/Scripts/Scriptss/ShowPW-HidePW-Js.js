87ded8e4eefb1fc1e0e8b3db8c4402e26b9d7991
PROJECTS-TESTING <33412315+PROJECTS-TESTING@users.noreply.github.com>
2018-04-19 13:38:49 +0700

    NOY46

---
+++ Exam_Objective/bin/Release/PublishOutput/Scripts/Scriptss/ShowPW-HidePW-Js.js
@@ -0,0 +1,13 @@
+﻿function CheckBlockPW(id) {
+    var x = document.getElementById(id);
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+function RandomPass(num, id) {
+    var pass = "";
+    for (var i = 0; i < num; i++) { pass += Math.floor(Math.random() * 10).toString() }
+    $(id).val(pass);
+}
